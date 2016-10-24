069f824e05edf9629ad1c3626c99ddfa82bc5b31
dbobkova <das-bobkova@yandex.ru>
2016-10-24 16:17:40 +0400

    Class Ball - main methods are created

---
+++ src/Ball.java
@@ -0,0 +1,64 @@
+/**
+ * Created by dbobkova on 24.10.2016.
+ */
+public class Ball {
+    //Methods
+
+
+    public Ball(float x, float y, int radius, int speed, int direction) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.xDelta = (float) (Math.cos(direction) * speed);
+        this.yDelta = (float) (Math.cos(direction) * (-speed));
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public float getxDelta() {
+        return xDelta;
+    }
+
+    public void setxDelta(float xDelta) {
+        this.xDelta = xDelta;
+    }
+
+    public float getyDelta() {
+        return yDelta;
+    }
+
+    public void setyDelta(float yDelta) {
+        this.yDelta = yDelta;
+    }
+
+
+
+    //Data
+    private float x; //coordinate of centre
+    private float y; //coordinate of centre
+    private int radius;
+    private float xDelta;
+    private float yDelta;
+}
