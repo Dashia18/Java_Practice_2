de306b0200010f1191b616fee7ee01f45e9887af
dbobkova <das-bobkova@yandex.ru>
2016-10-24 13:04:02 +0400

    Add class MyPolynomial

---
+++ src/MyPolynomial.java
@@ -0,0 +1,5 @@
+/**
+ * Created by dbobkova on 24.10.2016.
+ */
+public class MyPolynomial {
+}
--- src/Runner.java
+++ src/Runner.java
@@ -4,7 +4,7 @@
 public class Runner {
     public static void main(String[] args) {
 
-        
+
         //Class MyComplex
         MyComplex complexValue = new MyComplex(1.0, 1.0);
         System.out.println(complexValue.toString());
